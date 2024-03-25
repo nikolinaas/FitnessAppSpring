6d714338c256bbc3cccf3a3c88898abfd450233d
Nikolina Stojčić <stojcicn00@gmail.com>
2024-03-25 10:44:34 +0100

    m

---
+++ src/main/java/org/unibl/etf/ip/fitnessappspring/FitnessAppSpringApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.ip.fitnessappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/unibl/etf/ip/fitnessappspring/FitnessAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.ip.fitnessappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
